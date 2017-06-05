d927f9de6e7a1a813995150c7da2057e7141a855
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-05 11:59:44 -0500

    Implement the Collection View inside the TableView as per https://www.thorntech.com/2015/08/want-your-swift-app-to-scroll-in-two-directions-like-netflix-heres-how/ Next step is to connect our data source (Images) to the cell of CollectionView

--- Transportation/TransportCategoryRow.swift
+++ Transportation/TransportCategoryRow.swift
@@ -22,3 +22,30 @@ class TransportCategoryRow: UITableViewCell {
     }
 
 }
+
+extension TransportCategoryRow: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TransportationCell", for: indexPath)
+        
+        return cell
+    }
+}
+
+
+extension TransportCategoryRow: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let itemPerRow: CGFloat = 3
+        let hardCodedPadding: CGFloat = 5
+        let itemWidth = (collectionView.bounds.width / itemPerRow) - hardCodedPadding
+        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+}
